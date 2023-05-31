a1d27027c02ddf34f4aef6d068e9f6e9909fe5a2
Farhatyar <farhatyartamim290@gmail.com>
2023-05-31 11:52:21 -0400

    wed may 31 2023

---
+++ src/test/java/StepDefinitions/Hooks.java
@@ -0,0 +1,30 @@
+package StepDefinitions;
+
+import Utils.CommonMethods;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+public class Hooks extends CommonMethods {
+
+    @Before
+    public void preCondition(){
+        openBrowserAndLaunchApplication();
+
+    }
+ // Scenario class holds the complete information of your tests execution in Cucumber framework
+    @After
+public void postConditions(Scenario scenario){
+        byte[] pic;
+        if(scenario.isFailed()){
+           pic= takeScreenShot("failed/"+scenario.getName());
+        }else {
+          pic=  takeScreenShot("passed/"+scenario.getName());
+        }
+        // attach the screenshot in my report
+        scenario.attach(pic,"image/png",scenario.getName());
+
+        closeBrowser();
+
+}
+}
